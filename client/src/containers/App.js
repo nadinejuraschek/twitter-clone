e5693477419fddc1db42565b583b6d9192a5291b
Nadine Ju <nadine.jura@online.de>
2020-07-26 10:42:47 +0200

    redux store configured

---
+++ client/src/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '../store';
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        Hello
+      </div>
+    </Router>
+  </Provider>
+);
+
+export default App;
