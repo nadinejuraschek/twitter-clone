50787e08ce1126a750abe80c7b2655a9e2451de9
Nadine Ju <nadine.jura@online.de>
2020-09-15 19:53:34 +0200

    timeline now shows messages in reverse

--- client/src/containers/MessageList/index.js
+++ client/src/containers/MessageList/index.js
@@ -31,29 +31,37 @@ import MessageItem from '../../components/MessageItem';
 class MessageList extends Component {
   componentDidMount() {
     this.props.fetchMessages();
-  };
+  }
   render() {
     const { messages, removeMessage, currentUser } = this.props;
-    let messageList = messages.map(message => (
-      <MessageItem
-        key={message._id}
-        date={message.createdAt}
-        text={message.text}
-        username={message.user.username}
-        profileImageUrl={message.user.profileImageUrl}
-        removeMessage={removeMessage.bind(this, message.user._id, message._id)}
-        isCorrectUser={currentUser === message.user._id}
-      />
-    ));
-    return <ul className={styles.list}>{messageList}</ul>
-  };
-};
+    let messageList = messages
+      .reverse()
+      .map(message => (
+        <MessageItem
+          key={message._id}
+          date={message.createdAt}
+          text={message.text}
+          username={message.user.username}
+          profileImageUrl={message.user.profileImageUrl}
+          removeMessage={removeMessage.bind(
+            this,
+            message.user._id,
+            message._id
+          )}
+          isCorrectUser={currentUser === message.user._id}
+        />
+      ));
+    return <ul className={styles.list}>{messageList}</ul>;
+  }
+}
 
 function mapStateToProps(state) {
   return {
     messages: state.messages,
     currentUser: state.currentUser.user.id,
   };
-};
+}
 
-export default connect(mapStateToProps, { fetchMessages, removeMessage })(MessageList);
+export default connect(mapStateToProps, { fetchMessages, removeMessage })(
+  MessageList
+);
